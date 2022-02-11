fb18ab42e561a8eb28f8bb3c3ed5a0d894c25a1b
ArsalanAhmedarsal <arsalanahmedarsal@gmail.com>
2022-02-11 10:48:55 +0500

    Update Design

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,5 @@
-const preIcon = '<img src="img/slider-arrow-prev.png" alt="">';
-const nextIcon = '<img src="img/slider-arrow.png" alt="">';
+const preIcon = '<img src="img/slider-arrow-prev.png" alt="schliuf-img">';
+const nextIcon = '<img src="img/slider-arrow.png" alt="schliuf-img">';
 
 
 $('.listing-slider').owlCarousel({
@@ -7,31 +7,33 @@ $('.listing-slider').owlCarousel({
     margin: 10,
     nav: true,
     autoHeight: true,
+    thumbs: false,
     navText: [
         preIcon,
         nextIcon
 
     ],
     responsive: {
-        0:{
-            items:1
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        1000:{
-            items:3
+        1000: {
+            items: 3
         }
     }
 });
 
 
 $('.lable-slider').owlCarousel({
-   
+
     loop: true,
     margin: 10,
-    dots:false,
+    dots: false,
     nav: true,
+    thumbs: false,
     autoHeight: true,
     navText: [
         preIcon,
@@ -39,18 +41,73 @@ $('.lable-slider').owlCarousel({
 
     ],
     responsive: {
-        0:{
-            items:4.5
+        0: {
+            items: 4.5
         },
-        600:{
-            items:5.5
+        600: {
+            items: 5.5
         },
-        1000:{
-            items:6.5
+        1000: {
+            items: 6.5
         }
     }
 });
 
+
+
+
+
+
+$('.recently-view-slider').owlCarousel({
+
+    loop: true,
+    margin: 10,
+    dots: false,
+    nav: false,
+    autoHeight: true,
+    thumbs: false,
+    navText: [
+        preIcon,
+        nextIcon
+
+    ],
+    responsive: {
+        0: {
+            items: 1.5
+        },
+        600: {
+            items: 2.5
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
+
+
+
+$(document).ready(function () {
+    $('.detail-page-product-slider').owlCarousel({
+        loop: true,
+        items: 1,
+        dots: true,
+        slideSpeed: 2000,
+        autoplay: false,
+        thumbs: true,
+        thumbImage: true,
+        thumbContainerClass: 'owl-thumbs',
+        thumbItemClass: 'owl-thumb-item'
+    });
+
+
+
+});
+
+
+
+$(function () {
+    $("#tabs").tabs();
+});
 
 
 function NavigtionToggle() {
@@ -63,19 +120,40 @@ function NavigtionToggle() {
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-  }
+}
+
+
+
+//product plus Minus Counter
+
+$(document).ready(function () {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
 
